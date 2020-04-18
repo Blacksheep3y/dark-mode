730ffe1e7b11474204d6e9ce14f298dab0ac2e62
Matthew Castillo <blacksheep3y@gmail.com>
2020-04-17 22:31:16 -0400

    Finished Step 2 + 3

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import { useEffect} from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = enableDM => {
+    const [darkMode, setdarkMode] = useLocalStorage(enableDM)
+
+    useEffect(() => {
+        if (enableDM === enableDM){
+            console.log('DARKMODE ENABLED');
+            document.body.className = 'dark-mode';
+         }
+         else {
+            console.log('DARKMODE DISABLED');
+            return () => { document.body.className = ''; }
+         }
+    },[setdarkMode])
+    return [darkMode, setdarkMode];
+};
